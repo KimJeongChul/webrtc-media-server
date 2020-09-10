39b62bbb0725f8fd9317fc8608f9c38b85dffe57
jckim <kjc5443@gmail.com>
2020-09-10 13:21:05 +0900

    [adding] response message

---
+++ internal/websocket/responsemessage.go
@@ -0,0 +1,25 @@
+package websocket
+
+// ResponseMessage ...
+type ResponseMessage struct {
+	Method string `json:"method"`
+	RoomID string `json:"roomID"`
+	Status int    `json:"status"`
+}
+
+// createResMsgCreateRoom ...
+func createResMsgCreateRoom(roomID string) ResponseMessage {
+	resMsg := ResponseMessage{
+		RoomID: roomID,
+		Status: 200,
+	}
+	return resMsg
+}
+
+// createResMsgCreateRoom ...
+func createResMsgReleaseRoom() ResponseMessage {
+	resMsg := ResponseMessage{
+		Status: 200,
+	}
+	return resMsg
+}
