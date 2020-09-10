e09cce1e1590f8519ac058595e039699a1bf3876
jckim <kjc5443@gmail.com>
2020-09-10 16:38:03 +0900

    [update] rename page name

--- internal/net/web.go
+++ internal/net/web.go
@@ -9,7 +9,7 @@ import (
 // GET request Webpage serving
 func web(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles("sfu.html")
+		t, _ := template.ParseFiles("mediaserver.html")
 		err := t.Execute(w, nil)
 		if err != nil {
 			log.Println("[ERROR] Template Execute : ", err)
