0b0b9c41b8712df41d43eb4930da613911ca1480
KimJeongChul <kjc5443@gmail.com>
2020-09-30 19:56:33 +0900

    [update] rename function

---
+++ internal/net/js/main.js
@@ -0,0 +1,65 @@
+let server_addr = 'ws://localhost:8080/ws';
+var mediaserver = new WebSocket(server_addr)
+var media_room_id = null;
+var user_id = null;
+var handle_id = null;
+
+mediaserver.onopen = () => {
+    console.log('Media server ws open');
+}
+mediaserver.onclose = () => {
+    console.log('Media server ws closed');
+};
+mediaserver.onmessage = (msg) => {
+    const parsedMsg = JSON.parse(msg.data);
+    console.log("RECV msg : ", parsedMsg);
+    switch(parsedMsg.method) {
+        case "resCreateRoom": 
+        {
+            let inputMediaRoomID = document.getElementById('media_room_id');
+            inputMediaRoomID.value = parsedMsg.roomID;
+            media_room_id = parsedMsg.roomID;
+        }
+            break;
+        
+    }
+}
+
+//createHandle Create user and handle id
+function createHandle() {
+    user_id = uuidv4(); // Group
+    handle_id = makeid(8);// UserHandle
+    let inputUserID = document.getElementById('user_id');
+    inputUserID.value = user_id;
+    let inputHandleID = document.getElementById('handle_id');
+    inputHandleID.value = handle_id;
+}
+
+//makeCall Make a Call
+function makeCall() {
+    createHandle();
+
+    addRTCSession();
+}
+
+//releaseRoom Request create room
+function releaseRoom() {
+    const msgReleaseRoom = {
+        method: 'releaseRoom'
+    };
+    sendMsg(msgReleaseRoom);
+}
+
+//createRoom Request create room
+function createRoom() {
+    const msgCreateRoom = {
+        method: 'createRoom'
+    };
+    sendMsg(msgCreateRoom);
+}
+
+//sendMsg Send message websocket
+function sendMsg(msg) {
+    console.log("SEND msg : ", JSON.stringify(msg));
+    mediaserver.send(JSON.stringify(msg));
+}
