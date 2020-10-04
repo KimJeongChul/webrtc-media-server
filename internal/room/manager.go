45d93e395f8b0dbb4cb3b3d99920b09620c18e98
KimJeongChul <kjc5443@gmail.com>
2020-10-04 23:06:37 +0900

    [update] types room

--- internal/room/manager.go
+++ internal/room/manager.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"sync"
 
+	"github.com/KimJeongChul/webrtc-media-server/internal/types"
 	"github.com/KimJeongChul/webrtc-media-server/internal/utils"
 	"github.com/google/uuid"
 )
@@ -45,9 +46,9 @@ func (rm *RoomManager) Unregister(roomID string) {
 }
 
 // Load Load room by room ID.
-func (rm *RoomManager) Load(roomID string) (*Room, error) {
+func (rm *RoomManager) Load(roomID string) (*types.Room, error) {
 	if r, ok := rm.rooms.Load(roomID); ok {
-		room := r.(*Room)
+		room := r.(*types.Room)
 		return room, nil
 	} else {
 		return nil, errors.New("[error] the room doesn't exist")
