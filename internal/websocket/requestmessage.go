9a96178d759f3857a94bacc7ffba0398b8c2a551
KimJeongChul <kjc5443@gmail.com>
2020-10-04 22:38:15 +0900

    [update] requestmessage

--- internal/websocket/requestmessage.go
+++ internal/websocket/requestmessage.go
@@ -1,6 +1,20 @@
 package websocket
 
+import "github.com/pion/webrtc/v2"
+
+// SDP
+type Sdp struct {
+	Type string `json:"type"`
+	Sdp  string `json:"sdp"`
+}
+
+// RequestMessage
 type RequestMessage struct {
-	Method string `json:"method"`
-	RoomID string `json:"roomID"`
+	Method    string                  `json:"method"`
+	RoomID    string                  `json:"roomID"`
+	UserID    string                  `json:"userID"`
+	HandleID  string                  `json:"handleID"`
+	Sdp       Sdp                     `json:"sdp"`
+	Candidate webrtc.ICECandidateInit `json"candidate"`
+	MediaDir  string                  `json:"mediaDir"`
 }
