73ec84e9eaddb64a73a047db7b7d7c74dfbe5bc8
KimJeongChul <kjc5443@gmail.com>
2020-10-04 23:07:35 +0900

    [update] type room

--- internal/types/room.go
+++ internal/types/room.go
@@ -1,7 +1,14 @@
 package types
 
+// Room ...
+type Room interface {
+	Register(userID string, handleID string)
+}
+
+// RoomManager ...
 type RoomManager interface {
 	Destroy()
 	Register() string
 	Unregister(roomID string)
+	Load(roomID string) (*Room, error)
 }
