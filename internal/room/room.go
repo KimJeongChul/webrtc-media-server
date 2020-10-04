5f06730f8f3ae90c77ec86b021b9b145346d58a6
KimJeongChul <kjc5443@gmail.com>
2020-10-04 23:07:04 +0900

    [update] group, Register

--- internal/room/room.go
+++ internal/room/room.go
@@ -1,13 +1,25 @@
 package room
 
+import (
+	"sync"
+
+	"github.com/KimJeongChul/webrtc-media-server/internal/types"
+)
+
 type Room struct {
 	roomID string
+	group  *sync.Map
 }
 
-func NewRoom(roomID string) *Room {
+func NewRoom(roomID string) types.Room {
 	r := &Room{
 		roomID: roomID,
+		group:  new(sync.Map),
 	}
 
 	return r
+}
+
+func (r *Room) Register(userID string, handleID string) {
+	r.group.Store(userID, new(sync.Map))
 }
