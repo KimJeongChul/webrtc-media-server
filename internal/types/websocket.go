f2a0beaaba64a1b4dcf0ace01aeb1ec80a49a973
KimJeongChul <kjc5443@gmail.com>
2020-10-11 00:50:58 +0900

    [update] Send

--- internal/types/websocket.go
+++ internal/types/websocket.go
@@ -2,6 +2,11 @@ package types
 
 import "net/http"
 
+// WebSocket
+type WebSocket interface {
+	Send(v interface{}) error
+}
+
 // WebSocketHandler ...
 type WebSocketHandler interface {
 	Upgrade(w http.ResponseWriter, r *http.Request) error
