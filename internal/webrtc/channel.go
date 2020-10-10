19061c49516151b068fcdf31b3ac615fd2d6ce06
KimJeongChul <kjc5443@gmail.com>
2020-10-11 00:50:14 +0900

    [update] channel get set func

--- internal/webrtc/channel.go
+++ internal/webrtc/channel.go
@@ -1,21 +1,106 @@
 package webrtc
 
+import (
+	"sync"
+	"time"
+
+	"github.com/KimJeongChul/webrtc-media-server/internal/types"
+	"github.com/pion/webrtc/v2"
+)
+
+// Channel ...
 type Channel struct {
 	// Media Room Information
-	roomID   string
-	userID   string
-	handleID string
+	roomID         string
+	userID         string
+	handleID       string
+	mediaDirection string
+
+	// WebSocket
+	ws types.WebSocket
+
+	// PeerConnection
+	pc *webrtc.PeerConnection
+
+	// ICE connection state
+	iceConnState string
+
+	// RTCP
+	rtcpPLIInterval time.Duration
+
+	// Track
+	videoTrack *webrtc.Track
+	audioTrack *webrtc.Track
+
+	// Mutex
+	videoTrackLock *sync.RWMutex
+	audioTrackLock *sync.RWMutex
+
+	// Channel
+	videoRTCPQuit chan bool
 }
 
-type ChannelOption func(*Channel)
+// GetRoomID ...
+func (ch *Channel) GetRoomID() string {
+	return ch.roomID
+}
 
-func NewChannel(options ...func(*Channel)) *Channel {
-	channel := &Channel{}
+// GetUserID ...
+func (ch *Channel) GetUserID() string {
+	return ch.userID
+}
 
-	for _, option := range options {
-		option(channel)
-	}
+// GetHandleID ...
+func (ch *Channel) GetHandleID() string {
+	return ch.handleID
+}
 
-	//api.NewPeerConnection(peerConnectionConfig)
-	return channel
+// GetPeerConnection ...
+func (ch *Channel) GetPeerConnection() *webrtc.PeerConnection {
+	return ch.pc
+}
+
+// GetVideoTrack ...
+func (ch *Channel) GetVideoTrack() *webrtc.Track {
+	return ch.videoTrack
+}
+
+// GetAudioTrack ...
+func (ch *Channel) GetAudioTrack() *webrtc.Track {
+	return ch.audioTrack
+}
+
+// GetVideoTrackLock ...
+func (ch *Channel) GetVideoTrackLock() *sync.RWMutex {
+	return ch.videoTrackLock
+}
+
+// GetAudioTrackLock ...
+func (ch *Channel) GetAudioTrackLock() *sync.RWMutex {
+	return ch.audioTrackLock
+}
+
+// SetVideoTrack ...
+func (ch *Channel) SetVideoTrack(track *webrtc.Track) {
+	ch.videoTrack = track
+}
+
+// SetAudioTrack ...
+func (ch *Channel) SetAudioTrack(track *webrtc.Track) {
+	ch.audioTrack = track
+}
+
+// GetRTCPPLIInterval ...
+func (ch *Channel) GetRTCPPLIInterval() time.Duration {
+	return ch.rtcpPLIInterval
+}
+
+// GetVideoRTCPQuit ...
+func (ch *Channel) GetVideoRTCPQuit() chan bool {
+	return ch.videoRTCPQuit
+}
+
+// GetWebSocket ...
+func (ch *Channel) GetWebSocket() types.WebSocket {
+	return ch.ws
 }
